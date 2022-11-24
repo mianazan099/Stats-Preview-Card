b1b6088b75cdfdea25645ba45cbd4dbe34c516d7
mianazan099 <103571768+mianazan099@users.noreply.github.com>
2022-11-24 18:43:31 +0500

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
+    screens: {
+      d: "690px",
+    },
+    colors: {
+      DarkBlue: "hsl(233, 47%, 7%)",
+      darkDesaturatedBlue: "hsl(244, 38%, 16%)",
+      softViolet: "hsl(277, 64%, 61%)",
+      white: {
+        200: "hsla(0, 0%, 100%, 0.6)",
+        300: "hsla(0, 0%, 100%, 0.75)",
+        DEFAULT: "hsl(0, 0%, 100%)",
+      },
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      lexendDeca: ["Lexend Deca", "sans-serif"],
+    },
+    fontWeight: {
+      regular: "400",
+      bold: "700",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
